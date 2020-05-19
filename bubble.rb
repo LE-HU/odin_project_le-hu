0941ea981d8695aa46755661e911742597e3420c
LE-HU <wojciechlech1337@gmail.com>
2020-05-20 01:39:52 +0200

    added bubblesort script

---
+++ bubble.rb
@@ -0,0 +1,18 @@
+# bubble sort:)
+
+def bubble(array_to_sort)
+  swaps_this_run = array_to_sort.length # if this goes 0, we finished
+
+  for run in (1..array_to_sort.length) # defines number of algorithm runs
+    for position in (0...(array_to_sort.length - run)) # defines number of comparisons per run
+      if array_to_sort[position] > array_to_sort[position + 1]
+        # let's swap them if n > n+1
+        array_to_sort[position], array_to_sort[position + 1] = array_to_sort[position + 1], array_to_sort[position]
+      end
+    end
+  end
+
+  array_to_sort
+end
+
+print bubble([2, 5, 18, 4, 75, 1, 23, 26, 54, 32, 13])
