1107186ac4638bc96a3bdf26fd0572a4f6279add
LE-HU <wojciechlech1337@gmail.com>
2020-09-26 19:30:09 +0200

    add insert method to bst

--- take2/ruby/binary_search_tree.rb
+++ take2/ruby/binary_search_tree.rb
@@ -1,5 +1,6 @@
 # BST
-arr = Array.new(21).map { |x| x = rand(100) }
+# arr = Array.new(21).map { |x| x = rand(100) }.uniq.sort
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
 class Node
   attr_accessor :data, :left, :right
@@ -11,16 +12,48 @@ class Node
   end
 end
 
-def bst(arr)
-  return if arr.size == 1
+class Tree
+  attr_accessor :root
 
-  mid = arr.size / 2
+  def initialize(arr)
+    @arr = arr
+  end
 
-  root = Node.new(arr[mid])
+  def build_tree(arr = @arr)
+    return if arr.size == 1
 
-  root.left = bst(arr[0...mid])
-  root.right = bst(arr[mid..-1])
-  return root
+    mid = arr.size / 2
+    root = Node.new(arr[mid])
+
+    root.left = build_tree(arr[0...mid])
+    root.right = build_tree(arr[mid..-1])
+    @root = root
+    return root
+  end
+
+  def insert(val, root = @root)
+    if root.nil?
+      return @root = Node.new(val)
+    else
+      if root.data == val
+        return root
+      elsif root.data < val
+        root.right = insert(val, root.right)
+      else
+        root.left = insert(val, root.left)
+      end
+    end
+    @root = root
+  end
+
+  def pretty_print(node = @root, prefix = "", is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? "│   " : "    "}", false) if node.right
+    puts "#{prefix}#{is_left ? "└── " : "┌── "}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? "    " : "│   "}", true) if node.left
+  end
 end
 
-pp bst(arr)
+x = Tree.new(arr)
+x.build_tree
+x.insert(20)
+x.pretty_print
