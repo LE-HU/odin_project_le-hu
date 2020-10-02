a87c52c24478ed77047ea50c8c5e5b7de83ed085
LE-HU <wojciechlech1337@gmail.com>
2020-10-02 15:14:03 +0200

    tictactoe rspec tests refactor

--- take2/ruby/tictactoe/spec/tictactoe_spec.rb
+++ take2/ruby/tictactoe/spec/tictactoe_spec.rb
@@ -6,12 +6,15 @@ describe TicTacToe do
   before do
     allow($stdout).to receive(:write)
   end
+
+  let(:tictactoe) { TicTacToe.new }
+  let(:board) { get_board(tictactoe) }
+
   describe "#initialize" do
     it "initializes with 9x9 board and randomly selects starting player X/O" do
-      tictactoe = TicTacToe.new
       expect(tictactoe.turn_selector).to eq("X").or eq("O")
-      expect(get_board(tictactoe)).to include(".")
-      expect(get_board(tictactoe).size).to eq(9)
+      expect(board).to include(".")
+      expect(board.size).to eq(9)
     end
   end
 
@@ -22,19 +25,25 @@ describe TicTacToe do
 
   describe "player moves" do
     it "checks for valid player1 moves" do
-      tictactoe = TicTacToe.new
-      allow(tictactoe).to receive(:gets).and_return("1\n")
-      tictactoe.player1_move
-      board = get_board(tictactoe)
-      expect(board[1]).to eql("X")
-    end
-    it "checks for valid player1 moves" do
-      tictactoe = TicTacToe.new
       allow(tictactoe).to receive(:gets).and_return("0\n")
-      tictactoe.player2_move
-      board = get_board(tictactoe)
-      expect(board[0]).to eql("O")
+      tictactoe.player1_move
+      expect(board[0]).to eql("X")
     end
+    it "checks for valid player2 moves" do
+      allow(tictactoe).to receive(:gets).and_return("1\n")
+      tictactoe.player2_move
+      expect(board[1]).to eql("O")
+    end
+    it "check for turn_selector switches post player moves" do
+      allow(tictactoe).to receive(:gets).and_return("0\n")
+      tictactoe.player1_move
+      expect(tictactoe.turn_selector).to eql("O")
+      allow(tictactoe).to receive(:gets).and_return("1\n")
+      tictactoe.player2_move
+      expect(tictactoe.turn_selector).to eql("X")
+    end
+
+    # FIXME: FREEZES IN A LOOP.
     # it "checks for invalid player1 input" do
     #   tictactoe = TicTacToe.new
     #   allow(tictactoe).to receive(:gets).and_return("\n")
@@ -46,64 +55,48 @@ describe TicTacToe do
 
   describe "#winner?" do
     it "checks for win conditions case - horizontal first line" do
-      tictactoe = TicTacToe.new
-      board = get_board(tictactoe)
       board[0] = tictactoe.turn_selector
       board[1] = tictactoe.turn_selector
       board[2] = tictactoe.turn_selector
       expect(tictactoe.winner?).to be true
     end
     it "checks for win conditions case - horizontal second line" do
-      tictactoe = TicTacToe.new
-      board = get_board(tictactoe)
       board[3] = tictactoe.turn_selector
       board[4] = tictactoe.turn_selector
       board[5] = tictactoe.turn_selector
       expect(tictactoe.winner?).to be true
     end
     it "checks for win conditions case - horizontal third line" do
-      tictactoe = TicTacToe.new
-      board = get_board(tictactoe)
       board[6] = tictactoe.turn_selector
       board[7] = tictactoe.turn_selector
       board[8] = tictactoe.turn_selector
       expect(tictactoe.winner?).to be true
     end
     it "checks for win conditions case - diagonal top-down" do
-      tictactoe = TicTacToe.new
-      board = get_board(tictactoe)
       board[0] = tictactoe.turn_selector
       board[4] = tictactoe.turn_selector
       board[8] = tictactoe.turn_selector
       expect(tictactoe.winner?).to be true
     end
     it "checks for win conditions case - diagonal bottom-up" do
-      tictactoe = TicTacToe.new
-      board = get_board(tictactoe)
       board[0] = tictactoe.turn_selector
       board[4] = tictactoe.turn_selector
       board[8] = tictactoe.turn_selector
       expect(tictactoe.winner?).to be true
     end
     it "checks for win conditions case - vertical first line" do
-      tictactoe = TicTacToe.new
-      board = get_board(tictactoe)
       board[0] = tictactoe.turn_selector
       board[3] = tictactoe.turn_selector
       board[6] = tictactoe.turn_selector
       expect(tictactoe.winner?).to be true
     end
     it "checks for win conditions case - vertical second line" do
-      tictactoe = TicTacToe.new
-      board = get_board(tictactoe)
       board[1] = tictactoe.turn_selector
       board[4] = tictactoe.turn_selector
       board[7] = tictactoe.turn_selector
       expect(tictactoe.winner?).to be true
     end
     it "checks for win conditions case - vertical third line" do
-      tictactoe = TicTacToe.new
-      board = get_board(tictactoe)
       board[2] = tictactoe.turn_selector
       board[5] = tictactoe.turn_selector
       board[8] = tictactoe.turn_selector
@@ -112,9 +105,7 @@ describe TicTacToe do
   end
 
   describe "#draw?" do
-    it "checks for a draw when player marks occupy all board spaces" do
-      tictactoe = TicTacToe.new
-      board = get_board(tictactoe)
+    it "checks for a draw when player marks occupy all of board spaces" do
       board.map! { |spox| spot = tictactoe.turn_selector }
       expect(tictactoe.draw?).to be true
     end
