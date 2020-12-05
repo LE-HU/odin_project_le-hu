5e5a9833ce1b0b669a4489ccc2949a12d2d0012d
LE-HU <wojciechlech1337@gmail.com>
2020-12-05 16:38:28 +0100

    further TDD practice

--- take3/rspec_exercies/ruby_testing/spec/11b_cat_spec.rb
+++ take3/rspec_exercies/ruby_testing/spec/11b_cat_spec.rb
@@ -35,17 +35,20 @@ end
 
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
-
+  subject(:bobby) { described_class.new('Bobby', 'Kundel') }
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  context 'when Cat has the ability to talk' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(bobby).to_not be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(bobby).to be_hiding
   end
 end
--- take3/rspec_exercies/ruby_testing/spec/11c_dog_spec.rb
+++ take3/rspec_exercies/ruby_testing/spec/11c_dog_spec.rb
@@ -35,17 +35,20 @@ end
 
 describe Dog do
   # Create a subject with your choice of dog name and optional breed/color.
-
+  subject(:azor) { described_class.new(name: 'Azor', breed: "Wilczur", color: 'brown') }
   # Write a test using the second shared_example to test that dog responds to
   # talk ('WOOF!').
-  context '' do
+  context 'when Dog has the ability to talk' do
+    include_examples "shared method name"
   end
 
   # remove the 'x' before running this test
-  xit 'is not barking' do
+  it 'is not barking' do
+    expect(azor).not_to be_barking
   end
 
   # remove the 'x' before running this test
-  xit 'is sleeping' do
+  it 'is sleeping' do
+    expect(azor).to be_sleeping
   end
 end
